aa8c9c8cd0e6e890c6341c01c04e0696b93c627a
Quincy Quirrion <quincyquirrion@gmail.com>
2025-05-01 11:27:46 -0400

    homework

---
+++ In_class.py
@@ -0,0 +1,15 @@
+#define a function tht prints out hello world
+def helloWorld():
+    print ('Hello world!')
+
+print("Hi")
+helloWorld()
+
+# define a function which converts farenheit to celcius
+
+def tempconverter(degreFahrenheit):
+
+
+# define a function that prints out name
+
+
--- assignment_3.py
+++ assignment_3.py
@@ -8,7 +8,14 @@ which takes two parameters, adds them, and returns the sum
 
 '''
 
+def add_two_numbers(a,b):
 
+    return a + b
+
+print(add_two_numbers(1,1))
+print(add_two_numbers(2,2))
+print(add_two_numbers(3,3))
+print(add_two_numbers('a','b'))
 
 
 '''
@@ -22,8 +29,11 @@ if you do NOT pass a multiplier in it returns double the initial number
 If you pass in a multiplier it returns the first parameter multiplied by the second
 
 '''
+def multiplier(a,b):
+    return a * b
 
-
+print(multiplier(2,3))
+print(multiplier(2,))
 
 
 '''
@@ -35,5 +45,11 @@ It takes in two numbers (not 0 as the divisor until we reach exception handling 
 divides the first by the second and returns two values, the first is the result and the second is the remainder if it exists.
 
 '''
+def divider(a,b)
+    return a / b, a % b
+result, remainder = divider(2,2)
+
+print(f"result: {result}")
+print(f"remainder: {remainder}")
 
 
